fd1c6c1fa000c58d4b450150ad9458ec6b5125a1
Raphaelli Bellini <rafael.bellini@hotmail.com>
2020-11-18 18:13:14 -0300

    spring initializr

---
+++ src/main/java/br/com/uniacademia/escolabellini/EscolabelliniApplication.java
@@ -0,0 +1,13 @@
+package br.com.uniacademia.escolabellini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscolabelliniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscolabelliniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/uniacademia/escolabellini/EscolabelliniApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.uniacademia.escolabellini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscolabelliniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
